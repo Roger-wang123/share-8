212efff7c1962af79a26de795f22fc5f4369aecd
Roger <roger.wang@yamibuy.com>
2022-08-19 00:55:38 +0800

    A

--- src/script/srcset_ssr.ts
+++ src/script/srcset_ssr.ts
@@ -1,27 +1,34 @@
-import express from 'express'
-import { createSSRApp } from 'vue'
-import { renderToString } from 'vue/server-renderer'
+import express from "express";
+import { createSSRApp } from "vue";
+import { renderToString } from "vue/server-renderer";
 import { getImageSrcAndSrcsetCDNUrl } from "../utils";
 
-const server = express()
+const server = express();
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   const app = createSSRApp({
-    data: () => ({ mobileImgUrl: '//cdn.yamibuy.net/mkpl/733722a7fa6f83b7a060ca1868ed6ec4_0x0.png' }),
+    data: () => ({
+      mobileImgUrl:
+        "//cdn.yamibuy.net/mkpl/733722a7fa6f83b7a060ca1868ed6ec4_0x0.png",
+    }),
+    methods: {
+      getImageSrcAndSrcsetCDNUrl,
+    },
     template: `
     <div class="img-wrapper">
       <img
-        :src="getImageSrcAndSrcsetCDNUrl({ url: mobileImgUrl, width: '100%' })"
+        :src="getImageSrcAndSrcsetCDNUrl({ url: mobileImgUrl, width: '100%', scale: 0.563 })"
         :srcset="
           getImageSrcAndSrcsetCDNUrl({
             url: mobileImgUrl,
             width: '100%',
             type: 'srcset',
+            scale: 0.563
           })
         "
       />
-    </div>`
-  })
+    </div>`,
+  });
 
   renderToString(app).then((html) => {
     res.send(`
@@ -30,14 +37,23 @@ server.get('/', (req, res) => {
       <head>
         <title>srcset_ssr演示</title>
       </head>
+      <style>
+        *{
+          margin: 0;
+          padding: 0;
+        }
+        .img-wrapper, img {
+          width: 100%;
+        }
+      </style>
       <body>
         <div id="app">${html}</div>
       </body>
     </html>
-    `)
-  })
-})
+    `);
+  });
+});
 
 server.listen(3000, () => {
-  console.log('http://localhost:3000 ready')
-})
+  console.log("http://localhost:3000 ready~");
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -48,7 +48,7 @@ interface imageInfo {
   designWidth: number;
   scale: number; // 测试用  期望图片可以根据宽度自适应高度并返回
 }
-export const getImageSrcAndSrcsetCDNUrl = (data: imageInfo) => {
+export const getImageSrcAndSrcsetCDNUrl = (data: imageInfo): string => {
   if (!data.url)
     return "//cdn.yamibuy.net/statics/libraries/yamibuy/images/lazyloading.svg";
   let config: imageInfo = {
@@ -60,14 +60,15 @@ export const getImageSrcAndSrcsetCDNUrl = (data: imageInfo) => {
     designWidth: 375,
     scale: 1,
   };
+
+  config = <imageInfo>Object.assign(config, data);
   let sizeStr = "0x0",
     DIP = 375,
     DPR = 2; // 横向设备独立像素, 设备像素比
-  if (!data.ssr) {
+  if (!config.ssr) {
     DIP = window.innerWidth;
     DPR = window.devicePixelRatio;
   }
-  config = <imageInfo>Object.assign(config, data);
 
   if (config.type === "src") {
     // src应该设置为画质较低的图片 鉴于现在手机都是DPR=2起步的 所以使DPR=2 卡375*2这一档
@@ -79,10 +80,13 @@ export const getImageSrcAndSrcsetCDNUrl = (data: imageInfo) => {
       config.width.split("").pop() == "%"
     ) {
       const imgWidth =
-      (config.width.substr(0, config.width.length - 1) as any) * 0.01 * DIP * DPR;
+        (config.width.substr(0, config.width.length - 1) as any) *
+        0.01 *
+        DIP *
+        DPR;
       sizeStr = `${imgWidth}x${Math.floor(imgWidth * config.scale)}`;
     }
-    return cdnUrl(config.url, sizeStr);
+    return cdnUrl(config.url, sizeStr) as string;
   } else {
     // srcset 取比较主流的几个分辨率
     const level = [
